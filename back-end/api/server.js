ad59b0623c6a360fd9f3407ca7fe6702cf041792
Nelson Carvalho <nelsonfilth@hotmail.com>
2025-05-09 23:38:06 -0300

    ajuste no process novamente

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -9,7 +9,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-const frontEndDist = path.resolve(__dirname, "../front-end/dist");
+const frontEndDist = path.resolve(__dirname, "../../front-end/dist");
+
 
 const app = express();
 const PORT = 3000;
